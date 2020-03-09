7e571cc588da57651d197cbc3b7ef4cba2861ff8
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-09 19:03:12 -0400

    Added defaultProps and propTypes

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 function TechItem({ tech, onDelete }) {
   return (
     <li key={tech}>
@@ -7,6 +9,15 @@ function TechItem({ tech, onDelete }) {
       <button type="button" onClick={onDelete}>Delete</button>
     </li>
   );
+}
+
+TechItem.defaultProps = {
+  tech: 'Default Tech'
+}
+
+TechItem.propTypes = {
+  tech: PropTypes.string,
+  onDelete: PropTypes.func.isRequired,
 }
 
 export default TechItem;
